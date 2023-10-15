dac8862a9e069d8590f11f3bfb08a8bd2f678d52
sadikhossain69 <sadikhossain433@gmail.com>
2023-10-15 19:41:40 +0600

    file rearrage and homepage phpto download

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,9 @@ import { SiteHeader } from "@/components/site-header"
 import { ThemeProvider } from "@/components/theme-provider"
 import { useRoutes } from "react-router-dom"
 import { TailwindIndicator } from "./components/tailwind-indicator"
-import { ReactNode } from "react"
+import Home from "@/components/home/home"
 
 const routes = [{ path: "/", element: <Home /> }]
-
-function Home(): ReactNode {
-  return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Vite Ready.
-        </p>
-      </div>
-      <div></div>
-    </section>
-  )
-}
 
 function App() {
   const children = useRoutes(routes)
