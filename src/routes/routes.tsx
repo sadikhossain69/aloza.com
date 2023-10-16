6f7c3876ddf6a64d51ced57e0a015d7dd3815c67
sadikhossain69 <sadikhossain433@gmail.com>
2023-10-16 20:58:56 +0600

    router edited

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,11 @@
+import About from "@/components/common/about";
+import Contact from "@/components/contact";
 import Home from "@/components/home/home";
 
-const routes = [{ path: "/", element: <Home /> }]
+const routes = [
+    { path: "/", element: <Home /> },
+    { path: "/about", element: <About /> },
+    { path: "/contact", element: <Contact /> }
+]
 
 export default routes;
